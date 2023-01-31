0e2a7d495979abdceb3dd21b3418d1547959a548
Jérémie Piellard <jeremie.piellard@gmail.com>
2023-01-31 15:36:43 +0100

    Readme + introduction

---
+++ README.md
@@ -0,0 +1,6 @@
+# i18n
+This project is a silly tool to translate to and from numeronyms such as "i18n" (which stands for "internationalization"). The reverse translation illustrates how confusing such abbreviations can be.
+
+Only keep the first and last letters, and add the count of inner letters. Makes communication easier. M3s c11n e4r.
+
+[See it live here.](https://piellardj.github.io/i18n)
--- src/generate-page.ts
+++ src/generate-page.ts
@@ -6,7 +6,7 @@ const data = {
     title: "i18n",
     description: "Silly tool to translate to and from numeronyms such as \"i18n\".",
     introduction: [
-        "This is a silly tool to translate to and from numeronyms such as \"i18n\" (which stands for \"internationalization\").",
+        "This is a silly tool to translate to and from numeronyms such as \"i18n\" (which stands for \"internationalization\").  The reverse translation illustrates how confusing such abbreviations can be.",
         "Only keep the first and last letters, and add the count of inner letters. Makes communication easier. M3s c11n e4r."
     ],
     githubProjectName: "i18n",
